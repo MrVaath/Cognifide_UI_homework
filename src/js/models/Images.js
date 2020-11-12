05cf78dbe65ec3e83583e382850f79f19a81dbff
MrVaath <pat.wylegala@gmail.com>
2020-11-12 13:20:39 +0100

    Production build

--- src/js/models/Images.js
+++ src/js/models/Images.js
@@ -10,11 +10,10 @@ export default class Image {
       const res = await axios(
         `http://www.splashbase.co/api/v1/images/search?query=tree`
       );
+
       this.result = res.data.images;
-      // console.log(this.result);
     } catch (error) {
       console.log(error);
-      alert('Something went wrong');
     }
   }
 }
